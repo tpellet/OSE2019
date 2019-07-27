9ade1b221f3265afb3bc419913a49da36eee3a44
rccguest 2861 <rccguest2861@midway2-login2.rcc.local>
2019-07-27 13:15:00 -0500

    MPI day 3

--- day3/Exercise_day3/supplementary_material_mpi/allreduce.cpp
+++ day3/Exercise_day3/supplementary_material_mpi/allreduce.cpp
@@ -11,9 +11,13 @@ int main (int argc, char *argv[])
     MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    
+    my_rank = my_rank + 1;
 
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Reduce(&my_rank, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
+    //MPI_Comm_size(MPI_COMM_WORLD, &size);
+		
     /* Compute sum of all ranks. */
 
     printf ("Rank %i:\tSum = %i\n", my_rank, sum);
--- day3/Exercise_day3/supplementary_material_mpi/broadcast.cpp
+++ day3/Exercise_day3/supplementary_material_mpi/broadcast.cpp
@@ -6,14 +6,15 @@ int main(int argc, char *argv[])
     int rank, data;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int root_process = 0;
+
     if (rank==0) {
-        printf("enter a value:\n");
-        fflush(stdout);
-        scanf ("%d",&data);
+        data = 44;
     }
-
+	
     /* broadcast the value of data of rank 0 to all ranks */
-
+    MPI_Bcast(&data, 1, MPI_INT, root_process, MPI_COMM_WORLD);
     printf("I am rank %i and the value is %i\n", rank, data);
     MPI_Finalize();
     return 0;
